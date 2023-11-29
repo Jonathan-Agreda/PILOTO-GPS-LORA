e84c18fea014c4e0c064b040de4333546ff3fd75
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-11-29 00:30:07 -0500

    SE ACTUALIZA BDD DE TRACKIOT EN VPS

--- api/config/db.js
+++ api/config/db.js
@@ -1,8 +1,20 @@
-import mysql from "mysql2";
+/* import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+export default connection.promise(); */
+
+
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+    host: '135.148.121.160',
+    user: 'track_user',
+    password: 'trackIoT2023!!',
+    database: 'trackiot'
+});
 
 export default connection.promise();
